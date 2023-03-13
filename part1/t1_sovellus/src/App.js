be6bd3e554b9bfa229098e7b4fc0f3b68fb0cc07
Saukka <sauli.sorvari@kolumbus.fi>
2023-03-13 17:05:42 +0200

    t1.3

--- part1/t1_sovellus/src/App.js
+++ part1/t1_sovellus/src/App.js
@@ -1,16 +1,23 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const parts = [
-    {name: 'Fundamentals of React', exercises: 10},
-    {name: 'Using props to pass data', exercises: 7},
-    {name: 'State of a component', exercises: 14},
-  ]
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header name={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Content parts={[part1, part2, part3]}/>
+      <Total part1={part1} part2={part2} part3={part3}/>
     </div>
   )
 }
@@ -43,8 +50,9 @@ const Part = (part) => {
   )
 }
 
-const Total = ({parts}) => {
-  const number = parts[0].exercises + parts[1].exercises + parts[2].exercises
+const Total = (props) => {
+  console.log(props)
+  const number = props.part1.exercises + props.part2.exercises + props.part3.exercises
   return (
     <div>
       <p>Number of exercises {number}</p>
