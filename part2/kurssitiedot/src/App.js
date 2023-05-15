f6254df4bffb1caae2bae855071a7f477e1742ca
Saukka <sauli.sorvari@kolumbus.fi>
2023-05-15 18:19:57 +0300

    2.1 kurssitiedot step6

---
+++ part2/kurssitiedot/src/App.js
@@ -0,0 +1,69 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header text={course.name}/>
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({text}) => {
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
+}
+
+const Content = ({parts}) => {
+
+  return (
+    <div>
+      <ul>
+        {parts.map(part =>
+          <Part key={part.id} part={part}/>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+const Part = ({part}) => {
+  return (
+    <div>
+      <li>{part.name} {part.exercises}</li>
+    </div>
+  )
+}
+
+export default App
